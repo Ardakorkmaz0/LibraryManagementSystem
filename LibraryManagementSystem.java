e74f125e3128637dcaa9d3bd2b906f35f8271b9f
kerimcandede <kerimcandede631@gmail.com>
2025-11-27 21:26:43 +0300

    Add files via upload

---
+++ LibraryManagementSystem.java
@@ -0,0 +1,12 @@
+package librarymanagementsystem;
+
+public class LibraryManagementSystem {
+
+    public static void main(String[] args) {
+        LibraryManager lib = new LibraryManager(); // construct a library
+        System.out.printf("%35s" , "Main menu\nOperations :\n");
+        System.out.println("1 - Adding book to library");
+        
+    }
+    
+}
