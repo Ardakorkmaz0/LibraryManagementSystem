54625005db9fa66a3b5d6d681dcdda3c7b9c6c38
Arda6 <arda61qwer@hotmail.com>
2025-11-29 16:32:15 +0300

    redundant prints removed

--- src/LibraryManagementSystem.java
+++ src/LibraryManagementSystem.java
@@ -7,11 +7,6 @@ public class LibraryManagementSystem {
 
     public static void main(String[] args) {
         LibraryManager lib = new LibraryManager(); // construct a library
-        System.out.printf("%35s" , "--- Main menu ---\nOperations :\n");
-        System.out.println("1 - Adding book to library");
-
-
-
         Scanner sc = new Scanner(System.in);
 
         boolean isRunning = true;
